038605422d6a4982faf1c238a07879ef7e9256a2
rakhatama015 <132595654+rakhatama015@users.noreply.github.com>
2024-05-21 10:42:43 +0700

    Add files via upload

---
+++ TugasAkhir.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include 
+using namespace std;
+
+class Resto{
+    private:
+        
+    public:
+        void menuList();
+        void pesanMenu();
+        void tampilkanPesanan();
+        void reservasi();
+
+        void hash();
+        void sort();
+        void mergeSort();
+        void merge();
+
+        void queue();
+        void stack();
+};
+
+int main(){
+
+    return 0;
+}
+
+void Resto::menuList(){
+    cout << 
+}
